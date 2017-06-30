2b0d44270a275bc8cf3ae3a5edeb8b7d74c06584
Sergio Hidalgo <sergiohidalgo.94@gmail.com>
2017-06-30 03:45:57 -0400

    home

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,33 @@
 <template>
-  <div id="app">
+  <div id="app" class="wh100">
+      <div class="container h-100">
+          <div class="row h-100">
+              <div class="col-8 offset-4 d-flex align-items-center h-100">
+                  <div class="text-right">
+                      <h1>Encuentra a tu artista favorito</h1>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, quis!</p>
+                      <button class="btn btn-primary d-inline-flex align-items-center"><icon name="spotify" scale="2" class="mr-3"></icon> Ingresa con Spotify</button>
+                  </div>
+
+              </div>
+          </div>
+
+      </div>
+
+      <footer class="p-4">
+          <small>Proyecto sin fines de lucro, demo para Yapo &copy; 2017</small>
+      </footer>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+
+    import Icon from 'vue-awesome/components/Icon.vue'
+
+    export default {
+        name: 'app'
     }
-  }
-}
+
 </script>
 
 <style lang="sass">
--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
+
 import Vue from 'vue'
 import App from './App.vue'
+
+import 'vue-awesome/icons/spotify'
+
+import Icon from 'vue-awesome/components/Icon.vue'
+Vue.component('icon', Icon)
+
 
 new Vue({
   el: '#app',
   render: h => h(App)
 })
+
